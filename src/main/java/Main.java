5ffc343231dc6bc23101b5de895dd4e0dd874e00
Krzysztof Wrycza <wrycza.k@interia.pl>
2020-12-06 10:50:37 +0100

    Set Training done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("List Training");
+
         ListTraining.trainList();
+
+        System.out.println("Set Training");
+        SetTraining.trainSet();
     }
 }
